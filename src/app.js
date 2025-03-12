49cada8869f7365c8c837e395e9fb40b29f5066d
JorgeArevalo-hub <jorgearevalomoran@gmail.com>
2025-03-12 20:14:32 +0000

    excuseGenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuseFunction = (num1, num2, num3, num4) => {
+
+    const subject = ['a dog','a random kid','a policeman','my mom','the boss'];
+    const action = ['pooped in','stole','shot at','took away','destroyed'];
+    const target = ['my jacket','my car','my window','my food','my laptop'];
+    const time = ['yesterday','a few hours ago','in the last seconds','last week','this morning'];
+
+    let excuse = subject[num1] + " " + action[num2] + " " + target[num3] + " " + time[num4]
+    
+    return excuse
+  }
+  const generateRandomNumberBetween1And5 = () => Math.floor(Math.random() * (5 - 0) + 0);
+
+    const changeText = document.getElementById("excuse")
+
+     changeText.innerText = excuseFunction(generateRandomNumberBetween1And5(),generateRandomNumberBetween1And5(),generateRandomNumberBetween1And5(),generateRandomNumberBetween1And5());
+
 };
